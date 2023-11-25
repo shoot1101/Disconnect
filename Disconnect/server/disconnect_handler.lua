7491b177684abab8bfb997478f65bfe0d79c4a51
Bob Marly <33040669+shoot1101@users.noreply.github.com>
2023-11-25 17:03:19 +0100

    Update disconnect_handler.lua
    Add logs

--- Disconnect/server/disconnect_handler.lua
+++ Disconnect/server/disconnect_handler.lua
@@ -8,4 +8,17 @@ AddEventHandler('playerDropped', function (reason)
     local pCoords = GetEntityCoords(GetPlayerPed(source))
     cached_players[source] = {res = reason, name = GetPlayerName(source), coords = pCoords}
     TriggerClientEvent("utils:playerDisconnect", -1, source, {res = reason, name = GetPlayerName(source), pos = pCoords})
+    eLogsDiscord("[Disconnect] **"..xPlayer.getName().." a déco la raison est : "..reason, Config.logs.exit)
+end)
+
+---------------- LOGS
+function eLogsDiscord(message,url)
+    local DiscordWebHook = url
+    if message == nil or message == '' then return FALSE end
+    PerformHttpRequest(DiscordWebHook, function(err, text, headers) end, 'POST', json.encode({username = Config.logs.NameLogs, content = message}), { ['Content-Type'] = 'application/json' })
+end
+
+RegisterServerEvent('eExit:logsEvent')
+AddEventHandler('eExit:logsEvent', function(message, url)
+	eLogsDiscord(message,url)
 end)
